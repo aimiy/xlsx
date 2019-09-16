8825d0786a60aa02fc2f5def52d70932f5fcfd01
f <fuym@yuntongxun.com>
2019-09-16 17:14:43 +0800

    多个excle合并功能

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <div id="app">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Index />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Index from './views/index.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Index
   }
 }
 </script>
--- src/views/index.vue
+++ src/views/index.vue
@@ -2,10 +2,10 @@
   <div class="hello">
     源文件：
     <input type="file" @change="onFileChangeSrc" webkitdirectory />
-    目标文件：
-    <input type="file" @change="onFileChangeDest" />
+    <!-- 目标文件： -->
+    <!-- <input type="file" @change="onFileChangeDest" /> -->
     <button @click="confirm">生成</button>
-    <div class="container" ref="sheetContainer"></div>
+    <!-- <div class="container" ref="sheetContainer"></div> -->
   </div>
 </template>
 
@@ -18,29 +18,30 @@ export default {
     msg: String
   },
   data() {
-    return {};
+    return {
+      srcWorkBooks: {}, // 全部文件表格数据
+      srcSheets: [], // 全部文件sheets数据
+      srcSheetsJson: [], // 全部文件json数据
+      srcWorkBooksdealStatus:false, // json数据处理状态
+      destWorkBook: []
+    };
   },
   methods: {
     onFileChangeSrc(e) {
-      console.log(e);
       let files = e.target.files;
-      for (let file of files){
+      for (let file of files) {
         let reader = new FileReader();
         reader.onload = e => {
           let data = new Uint8Array(e.target.result);
           let workbook = XLSX.read(data, { type: "array" });
-          console.log(workbook);
+          this.srcWorkBooks[file.name] = workbook;
           let SheetNames = workbook.SheetNames;
-
           SheetNames.forEach(name => {
-           let worksheet = workbook.Sheets[name];
-
-           let data = XLSX.utils.sheet_to_json(worksheet);
-           console.log(data);
-           
-
-          })
-
+            let worksheet = workbook.Sheets[name];
+            this.srcSheets.push(worksheet);
+            let worksheetJdon = XLSX.utils.sheet_to_json(worksheet);
+            this.srcSheetsJson.push(...worksheetJdon);
+          });
         };
         reader.readAsArrayBuffer(file);
       }
@@ -52,32 +53,25 @@ export default {
       reader.onload = e => {
         let data = new Uint8Array(e.target.result);
         let workbook = XLSX.read(data, { type: "array" });
-        console.log(workbook);
-
+        this.destWorkBook = workbook;
         let first_sheet_name = workbook.SheetNames[0];
         let address_of_cell = "A1";
-
-        /* Get worksheet */
         let worksheet = workbook.Sheets[first_sheet_name];
-
-        let worksheetHTML = XLSX.utils.sheet_to_html(worksheet);
-
-        this.$refs["sheetContainer"].innerHTML = worksheetHTML;
-
-        console.log(this.sheetHtml);
-
-        /* Find desired cell */
+        this.showSheet(worksheet);
         let desired_cell = worksheet[address_of_cell];
-
-        /* Get the value */
         let desired_value = desired_cell ? desired_cell.v : undefined;
-
-        /* DO SOMETHING WITH workbook HERE */
       };
       reader.readAsArrayBuffer(f);
     },
-    confirm(){
-
+    confirm() {
+      let allDataSheet = XLSX.utils.json_to_sheet(this.srcSheetsJson);
+      let allDataBook = XLSX.utils.book_new();
+      XLSX.utils.book_append_sheet(allDataBook, allDataSheet, "汇总表");
+      XLSX.writeFile(allDataBook, "汇总表.xlsx");
+    },
+    showSheet(sheetData) {
+      let worksheetHTML = XLSX.utils.sheet_to_html(sheetData);
+      this.$refs["sheetContainer"].innerHTML = worksheetHTML;
     }
   },
   mounted() {}
